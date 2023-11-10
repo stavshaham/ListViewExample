8585df83d84625a5e66db25b60e1467052c4a910
מחשבים 1 <lab1@oded.local>
2023-11-10 10:37:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.listviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> asd = new ArrayList<>();
+    private ArrayAdapter<String> aad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        asd.add("asd");
+        aad = new ArrayAdapter<>(this, R.layout.items, asd);
+
+        listView.setAdapter(aad);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
+    }
+}
